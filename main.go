db32aab2d2745e35bdb7d7f93d8ef7373f155778
luzhong <luzhong@zeaho.com>
2021-11-30 16:19:29 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/BeanCookie/magic-box-api/models"
+	"github.com/BeanCookie/magic-box-api/pkg/gredis"
+	"github.com/BeanCookie/magic-box-api/pkg/logging"
+	"github.com/BeanCookie/magic-box-api/pkg/setting"
+	"github.com/BeanCookie/magic-box-api/routers"
+	"github.com/BeanCookie/magic-box-api/pkg/util"
+)
+
+func init() {
+	setting.Setup()
+	models.Setup()
+	logging.Setup()
+	gredis.Setup()
+	util.Setup()
+}
+
+// @title Golang Gin API
+// @version 1.0
+// @description An example of gin
+// @termsOfService https://github.com/BeanCookie/magic-box-api
+// @license.name MIT
+// @license.url https://github.com/BeanCookie/magic-box-api/blob/master/LICENSE
+func main() {
+	gin.SetMode(setting.ServerSetting.RunMode)
+
+	routersInit := routers.InitRouter()
+	readTimeout := setting.ServerSetting.ReadTimeout
+	writeTimeout := setting.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%d", setting.ServerSetting.HttpPort)
+	maxHeaderBytes := 1 << 20
+
+	server := &http.Server{
+		Addr:           endPoint,
+		Handler:        routersInit,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+	}
+
+	log.Printf("[info] start http server listening %s", endPoint)
+
+	server.ListenAndServe()
+
+
+}
---
+++ routers/router.go
@@ -0,0 +1,47 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/BeanCookie/magic-box-api/docs"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	"github.com/BeanCookie/magic-box-api/middleware/jwt"
+	"github.com/BeanCookie/magic-box-api/pkg/export"
+	"github.com/BeanCookie/magic-box-api/pkg/qrcode"
+	"github.com/BeanCookie/magic-box-api/pkg/upload"
+	"github.com/BeanCookie/magic-box-api/routers/api"
+	"github.com/BeanCookie/magic-box-api/routers/api/v1"
+)
+
+// InitRouter initialize routing information
+func InitRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	r.POST("/auth", api.GetAuth)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
+	{
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+		//生成文章海报
+		apiv1.POST("/articles/poster/generate", v1.GenerateArticlePoster)
+	}
+
+	return r
+}
