548f04ddce811dfd698c750b4e082f002cbc69fe
鲁众 <loveluzhong@outlook.com>
2021-12-15 15:17:04 +0800

    优化掘金爬虫

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/BeanCookie/magic-box-api/pkg/setting"
+	"github.com/BeanCookie/magic-box-api/service/csdn_service"
 	"github.com/BeanCookie/magic-box-api/service/juejin_service"
 	"github.com/robfig/cron/v3"
 	"github.com/rs/zerolog/log"
@@ -13,15 +14,16 @@ type ArticleJob struct{}
 
 func (j ArticleJob) Run() {
 	juejin := setting.ThirdPartysSetting.JueJin
-	// csdn := setting.ThirdPartysSetting.Csdn
+	csdn := setting.ThirdPartysSetting.Csdn
 	juejin_service.ParseArticles(juejin)
-	// csdn_service.ParseArticles("https://blog.csdn.net/phoenix/web/blog/hot-rank?page=0&pageSize=25")
+	csdn_service.ParseArticles(csdn)
 
 	log.Info().Msgf("run article job %v", time.Now())
 }
 
 func Setup() {
 	c := cron.New(cron.WithSeconds())
+	ArticleJob{}.Run()
 	c.AddJob(setting.AppSetting.ScheduleCron, ArticleJob{})
 	c.Start()
 }
